e559418466f55d52bd6b5aad519c9d51a5f4e6db
lion.71 <lion.71@inbox.ru>
2023-02-25 19:37:53 +0300

    Добавлена програма

--- Exzamples/Exzample005_IfElse/Program.cs
+++ Exzamples/Exzample005_IfElse/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите своё имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура,это же Маша!!!");
+}
+else
+{
+    Console.WriteLine("Привет,");
+    Console.WriteLine(username);
+}
