a9dd8a85e77f2ded270372e0df845301a5e93eed
lion.71 <lion.71@inbox.ru>
2023-02-28 21:03:22 +0300

    Prijekt009

--- Exzamples/Exzample009/Program.cs
+++ Exzamples/Exzample009/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] array = { 1, 12, 23, 4, 15, 56, 77, 18, 29};
+
+int n = array.Length;
+int find = 23;
+int index = 0;
+while(index < n)
+{
+    if(array[index]==find)
+    Console.WriteLine(index);
+    index++;
+}
